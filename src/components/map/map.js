55391aab2b93ea40212345e31834bf6c87bd14d7
Justin <jtv6445@g.rit.edu>
2020-04-26 19:10:13 -0700

    Includes react demo

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Map = (props) => {
   // Any functions or state can go here
+  const [myNumber, setMyNumber] = useState(0);
+  const [notMyNumber, setNotMyNumber] = useState(-34);
 
   return (
     <div className='match-map'>
-      {/* Do whatever you gotta do here */}
+      {myNumber}
+      {notMyNumber}
+      <button onClick={(e) => {setMyNumber(myNumber+1)}}>
+        click me
+      </button>
     </div>
   )
 }
+
+export default Map;
